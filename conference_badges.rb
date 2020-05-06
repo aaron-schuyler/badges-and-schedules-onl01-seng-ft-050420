bce86e42e47fe548e7257ab3241291e31a7dbd80
Aaron Schuyler <asky28@gmail.com>
2020-05-06 14:21:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,13 +1,17 @@
 def batch_badge_creator(names)
+  arr = []
   names.each do |name|
-    puts "Hello my name is #{name}."
+    arr.push("Hello my name is #{name}.")
   end
+  return arr
 end
 
 def assign_rooms(names)
+  arr = []
   room = 1
   names.each do |name|
-    return "Hello #{name}! You'll be assigned to room #{room}!"
+    arr.push("Hello #{name}! You'll be assigned to room #{room}!")
     room+=1
   end
+  return arr
 end
