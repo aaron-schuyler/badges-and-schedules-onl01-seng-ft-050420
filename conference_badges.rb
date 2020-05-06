3430db5696aa772b5aa40cdb9658738065368407
Aaron Schuyler <asky28@gmail.com>
2020-05-06 14:32:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,10 +9,10 @@ def badge_maker(name)
 end
 
 def assign_rooms(names)
-  room = 1
+  room = 0
   names.collect do |name|
-    "Hello, #{name}! You'll be assigned to room #{room}!"
     room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
   end
 end
 
